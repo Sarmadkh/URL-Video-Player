1595344336dfecc6c4d63c855e42d0695ee927a3
Sarmadkh <mskb59@gmail.com>
2024-05-20 21:43:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,65 +12,48 @@ function playVideo() {
     var videoUrl = document.getElementById('videoUrl').value;
     var videoPlayer = document.getElementById('videoPlayer');
     var videoTitle = document.getElementById('videoTitle');
-    
-    if (isValidURL(videoUrl)) {
-        videoPlayer.src = videoUrl;
-        videoPlayer.play();
-        
-        var fileName = extractFileName(videoUrl);
-        var showName = fileName.split(' - ')[0];
-        document.title = showName || fileName;
 
-        videoTitle.textContent = fileName;
-    } else {
-        alert('Please enter a valid video URL.');
-    }
+    videoPlayer.src = videoUrl;
+    videoPlayer.play();
+
+    var fileName = extractFileName(videoUrl);
+    var showName = fileName.split(' - ')[0];
+    document.title = showName || fileName;
+
+    videoTitle.textContent = fileName;
 }
 
 function playHlsVideo() {
     var hlsUrl = document.getElementById('hlsUrl').value;
     var videoPlayer = document.getElementById('videoPlayer');
     var videoTitle = document.getElementById('videoTitle');
-    
-    if (isValidURL(hlsUrl)) {
-        if (Hls.isSupported()) {
-            var hls = new Hls();
-            hls.loadSource(hlsUrl);
-            hls.attachMedia(videoPlayer);
-            hls.on(Hls.Events.MANIFEST_PARSED, function() {
-                videoPlayer.play();
-            });
-        } else if (videoPlayer.canPlayType('application/vnd.apple.mpegurl')) {
-            videoPlayer.src = hlsUrl;
+
+    if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(hlsUrl);
+        hls.attachMedia(videoPlayer);
+        hls.on(Hls.Events.MANIFEST_PARSED, function() {
             videoPlayer.play();
-        }
-
-        var fileName = extractFileName(hlsUrl);
-        var showName = fileName.split(' - ')[0];
-        document.title = showName || fileName;
-
-        videoTitle.textContent = fileName;
-    } else {
-        alert('Please enter a valid HLS (m3u8) URL.');
+        });
+    } else if (videoPlayer.canPlayType('application/vnd.apple.mpegurl')) {
+        videoPlayer.src = hlsUrl;
+        videoPlayer.play();
     }
-}
 
-function isValidURL(url) {
-    var pattern = new RegExp('^(https?:\\/\\/)?'+
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+
-    '(\\#[-a-z\\d_]*)?$','i');
-    return !!pattern.test(url);
+    var fileName = extractFileName(hlsUrl);
+    var showName = fileName.split(' - ')[0];
+    document.title = showName || fileName;
+
+    videoTitle.textContent = fileName;
 }
 
 function extractFileName(url) {
-    var urlObj = new URL(url);
+    var urlObj = new URL(decodeURIComponent(url));
     var pathSegments = urlObj.pathname.split('/');
     var fileName = pathSegments[pathSegments.length - 1];
-    
+
     fileName = fileName.replace(/\./g, ' ');
+    fileName = fileName.replace(/%20/g, ' '); // Replace %20 with space
 
     var seasonEpisodePattern = /(S\d{2}E\d{2})/i;
     var match = fileName.match(seasonEpisodePattern);
@@ -111,3 +94,4 @@ function extractFileName(url) {
 
     return showName + ' - ' + episodeNumber + ' - ' + episodeName;
 }
+
